4c7333084b7b8522d4ca35ed06ab4d333d358e0a
Esteban Salas <juan.salasf@udea.edu.co>
2024-09-30 00:31:40 -0500

    feat: función para imprimir array de strings

--- reverse.c
+++ reverse.c
@@ -7,14 +7,29 @@
 #define MAX_LINE_LENGTH 1024
 
 // Verificar si dos archivos son iguales
-int same_file(const char *file1, const char *file2) {
+int same_file(const char *file1, const char *file2)
+{
     struct stat stat1, stat2;
-    if (stat(file1, &stat1) != 0 || stat(file2, &stat2) != 0) {
+    if (stat(file1, &stat1) != 0 || stat(file2, &stat2) != 0)
+    {
         return -1;
     }
-    if (stat1.st_dev == stat2.st_dev && stat1.st_ino == stat2.st_ino) {
+    if (stat1.st_dev == stat2.st_dev && stat1.st_ino == stat2.st_ino)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
+    }
+}
+
+// Imprimir un array de strings
+void print_array(char **array)
+{
+    while (*array != NULL)
+    {
+        printf("%s", *array);
+        array++;
     }
 }
