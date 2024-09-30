3a8886a6fecff5112a669cd3781c7bca7d41ab46
cbasguti <byoxxill@gmail.com>
2024-09-30 00:30:57 -0500

    feat: agregar funcion de comparacion de archivo

--- reverse.c
+++ reverse.c
@@ -5,3 +5,16 @@
 
 // Definir la longitud máxima de una línea
 #define MAX_LINE_LENGTH 1024
+
+// Verificar si dos archivos son iguales
+int same_file(const char *file1, const char *file2) {
+    struct stat stat1, stat2;
+    if (stat(file1, &stat1) != 0 || stat(file2, &stat2) != 0) {
+        return -1;
+    }
+    if (stat1.st_dev == stat2.st_dev && stat1.st_ino == stat2.st_ino) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
